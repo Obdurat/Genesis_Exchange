575ed9c0bec779dfdb6bc663f056961f09bd189d
Obdurat <omicron_rpl@hotmail.com>
2023-04-05 21:02:45 -0300

    feat: Added new column to exchanges table called conversion_result

--- database_cfg/z_create_exchanges.sql
+++ database_cfg/z_create_exchanges.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS genesis.exchanges (
   from_currency INTEGER NOT NULL,
   to_currency INTEGER NOT NULL,
   rate DECIMAL(15, 2) NOT NULL,
+  conversion_result DECIMAL(15, 2) NOT NULL,
   FOREIGN KEY (from_currency) REFERENCES currency(id),
   FOREIGN KEY (to_currency) REFERENCES currency(id)
 )
