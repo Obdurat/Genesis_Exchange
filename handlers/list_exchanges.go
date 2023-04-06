1bdd31cf11afbce413b67318f2cc69b75722991a
Obdurat <omicron_rpl@hotmail.com>
2023-04-05 21:05:40 -0300

    feat: Added List handler to get all exchanges

---
+++ handlers/list_exchanges.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Obdurat/genesis/repository"
+	"github.com/gin-gonic/gin"
+)
+
+func ListExchanges(c *gin.Context) {
+	results, err := repository.List(); if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()}); return
+	}
+	c.JSON(http.StatusOK, results)
+}
