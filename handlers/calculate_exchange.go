6a4182a1d3ebe56d6fa547f2cb97de778c3c115e
Obdurat <omicron_rpl@hotmail.com>
2023-04-05 21:03:51 -0300

    feat: Added From parameter validation on Calc Exchange function

--- handlers/calculate_exchange.go
+++ handlers/calculate_exchange.go
@@ -4,23 +4,31 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Obdurat/genesis/repository"
 	"github.com/gin-gonic/gin"
 )
 
 func CalcExchange(c *gin.Context) {
 	amount, from, to, rate := c.Param("amount"), c.Param("from"), c.Param("to"), c.Param("rate")
-	println(from)
 	original, err := strconv.ParseFloat(amount, 2); if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid amount parameter", "message": err.Error()}); return
 	}
 	conversion_rate, err := strconv.ParseFloat(rate, 2); if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid amount parameter", "message": err.Error()}); return
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid rate parameter", "message": err.Error()}); return
+	}
+	_, err = CurrencySim(from); if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return
 	}
 	simbol, err := CurrencySim(to); if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return
 	}
+
+	result := original * conversion_rate
+
+	go repository.Save(original, from, to ,conversion_rate, result);
+
 	c.JSON(http.StatusOK, &Reponse{
-		Result: original * conversion_rate,
+		Result: result,
 		Simbol: simbol,
 	})
 }
