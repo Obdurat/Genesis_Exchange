3cc7b0421ab3d965ba6905b0710c6ee97940d2d9
Obdurat <omicron_rpl@hotmail.com>
2023-04-05 21:11:56 -0300

    fix: Fix list endpoint and removed unescessary dockerignore file

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.POST("/exchange/:amount/:from/:to/:rate", handlers.CalcExchange)
-	r.GET("exchanges/list", handlers.ListExchanges)
+	r.GET("/exchange/list", handlers.ListExchanges)
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
