d6f3be49fd3526114d079a9446959ffeb0537001
Obdurat <omicron_rpl@hotmail.com>
2023-04-05 21:07:58 -0300

    refactor: refactor main to register routes and now the Repository call and closes the DB connection

--- main.go
+++ main.go
@@ -4,18 +4,14 @@ import (
 	"log"
 
 	"github.com/Obdurat/genesis/handlers"
-	"github.com/Obdurat/genesis/infra"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db := infra.DB
-	if err := db.Connect("mysql", "root:12345678@tcp(localhost:3306)/genesis_logs"); err != nil {
-		panic(err)
-	}
 	r := gin.Default()
-	r.GET("/exchange/:amount/:from/:to/:rate", handlers.CalcExchange)
+	r.POST("/exchange/:amount/:from/:to/:rate", handlers.CalcExchange)
+	r.GET("exchanges/list", handlers.ListExchanges)
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
