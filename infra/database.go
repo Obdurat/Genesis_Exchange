f50850c54c28384481ea82f26639642b146815dd
Obdurat <omicron_rpl@hotmail.com>
2023-04-13 02:07:19 -0300

    refactor: Query and Exec methods are no longer nescessary on the Infra layer because we get a new connection from the pool

--- infra/database.go
+++ infra/database.go
@@ -1,6 +1,9 @@
 package infra
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 func (d *Datasource) Connect(driver string, source string) error {
 	db, err := sql.Open(driver, source); if err != nil {
@@ -13,25 +16,15 @@ func (d *Datasource) Connect(driver string, source string) error {
 	return nil
 }
 
-func (d *Datasource) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	rows, err := d.Source.Query(query, args...); if err != nil {
-		return nil, err
-	}
-	return rows, nil
-}
-
-func (d *Datasource) Exec(query string, args ...interface{}) (sql.Result, error) {
-	result, err := d.Source.Exec(query, args...); if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
 func (d *Datasource) Disconnect() error {
 	if err := d.Source.Close(); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (d *Datasource) GetConn(ctx context.Context) (*sql.Conn, error) {
+	return d.Source.Conn(ctx)
 }
 
 func New() IDatasource {
--- infra/protocols.go
+++ infra/protocols.go
@@ -1,6 +1,9 @@
 package infra
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type Datasource struct {
 	Source *sql.DB
@@ -8,7 +11,6 @@ type Datasource struct {
 
 type IDatasource interface {
 	Connect(driver string, source string) error
-	Query(query string, args ...interface{}) (*sql.Rows, error)
-	Exec(query string, args ...interface{}) (sql.Result, error)
+	GetConn(ctx context.Context) (*sql.Conn, error)
 	Disconnect() error
 }
